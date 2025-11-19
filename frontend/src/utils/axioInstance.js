974c31ea4f175ce71a427b2c83cfd8f4d86a66f1
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2025-11-19 21:27:10 +0530

    Update axioInstance.js

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://taskmanagerbackend-m5sc.onrender.com"
+const BASE_URL = "http://localhost:3000/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
